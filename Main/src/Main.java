c67cb624126d72e4d4de3871c5e2b8373d5bae3d
Dushy <dushyantverma.cs11@gmail.com>
2024-06-01 23:40:43 +0530

    first commit

---
+++ Main/src/Main.java
@@ -0,0 +1,16 @@
+
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        //Variables
+        int a,b;
+        a=25;
+        b=10;
+        int sum = a+b;
+//        System.out.println(sum);
+
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next();
+        System.out.println(name);
+    }
+}
